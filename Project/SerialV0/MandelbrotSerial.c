a2668d0f70134f8ab75ffeb72849d8be05c9716e
Ferran de Miguel <ferran.de.miguel@estudiantat.upc.edu>
2023-04-03 01:47:02 +0200

    Added OMP to the code

--- Project/SerialV0/MandelbrotSerial.c
+++ Project/SerialV0/MandelbrotSerial.c
@@ -1,21 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 #include "Image.h"
 #define ac_mat(X,i,j,n) X[(i)*(n)+(j)]
 //Posar timing del calcul
-int main(){
-    const int n=5000;
-    const int iter=1000;
+int main(int argc, char *argv[]){
+    clock_t time1, time2;
+    double dub_time;
+    const int n=atoi(argv[1]);  //Up to n=40000 using 16Gb RAM and a little bit of swap
+    const int iter=atoi(argv[2]);
     const int ch=1;
-    const double WindowX[2]={-2,1};
-    const double WindowY[2]={-1, 1};
+    const double WindowX[2]={-2,0.47};
+    const double WindowY[2]={-1.12, 1.12};
     const double deltax=(WindowX[1]-WindowX[0])/(double)(n-1);
     const double deltay=(WindowY[1]-WindowY[0])/(double)(n-1);
-    double coordX[n]; //Can be arrays, not changing potser no, el heap? es petit
-    double coordY[n];
+    double* coordX=(double*)calloc(n,sizeof(double)); //Can be arrays, not changing potser no, el heap? es petit
+    double* coordY=(double*)calloc(n,sizeof(double));
     double* k=(double*)calloc(n*n,sizeof(double)); //Pot ser un static array
-
+    double* k2=(double*)calloc(n*n,sizeof(double)); 
 
     coordX[0]=WindowX[0];
     coordY[0]=WindowY[0];
@@ -25,8 +28,8 @@ int main(){
         coordX[i]=coordX[0]+i*deltax;
         coordY[i]=coordY[0]+i*deltay;
     }
-    
-
+    //Naive code
+    time1=clock();
     //Compute the mandelbrot set
     for (int px = 0; px < n; ++px)
     {   
@@ -39,7 +42,7 @@ int main(){
             double zy=0.;
             double zz=0.;
             //printf("X:%d Y:%d ",px,py);
-            for (int i = 1; i < iter+1; ++i)
+            for (int i = 1; i < iter+1; ++i) //To be the same as the matlab code
             {
                 double zx2=cx+zx*zx-zy*zy;
                 double zy2=cy+zx*zy*2;
@@ -56,8 +59,46 @@ int main(){
             ac_mat(k,px,py,n) = zz;
             
         }
-        
     }
+    time2=clock();
+    dub_time = (time2 - time1)/(double) CLOCKS_PER_SEC;
+    printf("Time for n=%d and iter=%d -----> %lf \n", n,iter,dub_time);
+
+    //Optimized code
+    time1=clock();
+    //Compute the mandelbrot set
+    for (int px = 0; px < n; ++px)
+    {   
+        double x0=coordX[px]; //Posar dintre altre for per OPEN*
+        for (int py = 0; py < n; ++py)
+        {
+            
+            double y0=coordY[py];
+            double x2=0;
+            double y2=0;
+            double x=0;
+            double y=0;
+            //printf("X:%d Y:%d ",px,py);
+            int iteration=0;
+            while (x2+y2<=4 && iteration<iter)
+            {
+                y=(x+x)*y+y0;
+                x=x2-y2+x0;
+                x2=x*x;
+                y2=y*y;
+                iteration++;
+            }
+            
+            ac_mat(k2,px,py,n) = iter-iteration;
+            
+        }
+    }
+    time2=clock();
+    dub_time = (time2 - time1)/(double) CLOCKS_PER_SEC;
+    printf("Time for n=%d and iter=%d optimized -----> %lf \n", n,iter,dub_time);
+
+    
+
     #ifdef DEBUG
         for (int px = 0; px < n; px++)
         {
@@ -67,6 +108,19 @@ int main(){
             }
             printf("\n");
         }
+        printf("Mat2\n");
+        for (int px = 0; px < n; px++)
+        {
+            for (int py = 0; py < n; py++)
+            {
+                printf("%f ", ac_mat(k2,px,py,n));
+            }
+            printf("\n");
+        }
     #endif
     writeImage("test",k,n,n,ch);
+    writeImage("test2",k2,n,n,ch);
+
+    free(k);
+    free(k2);
 }
--- Project/view.py
+++ Project/view.py
@@ -7,7 +7,7 @@ from __future__ import print_function, division
 
 import os, sys, numpy as np
 import matplotlib.pyplot as plt
-
+import time
 
 imgfile = sys.argv[1]
 
@@ -16,13 +16,14 @@ info = np.genfromtxt(imgfile+'.info',comments='#')
 ch   = int(info[0]) # Number of channels
 w    = int(info[1]) # Width
 h    = int(info[2]) # Heigth
+while True:
+    # Load image data - double array
+    data = np.fromfile(imgfile+'.bin').reshape(h,w,ch)
 
-# Load image data - double array
-data = np.fromfile(imgfile+'.bin').reshape(h,w,ch)
+    # Convert to image (uint8)
+    img = np.clip(data*(1<<8-1),0,1<<8-1).astype(np.uint8)
 
-# Convert to image (uint8)
-img = np.clip(data*(1<<8-1),0,1<<8-1).astype(np.uint8)
-
-# Show image
-plt.imshow(img)
-plt.show()
+    # Show image
+    plt.imshow(img)
+    plt.show()
+    time.sleep(10)
